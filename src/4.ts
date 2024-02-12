36825e61b287d384e684526e2b6bb4c994fd886f
Holubchuk <sashagolubchuk3@gmail.com>
2024-02-11 20:47:10 -0500

    feature/first-try

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,63 @@
+class Key {
+  private signature: number;
+  
+  constructor(signature: number) {
+    this.signature = Math.round(signature);
+    
+  }
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  private key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  door: boolean;
+  key: Key;
+  tenants: Person[];
+
+  constructor(key: Key) {
+    this.door = false;
+    this.key = key;
+    this.tenants = [];
+  }
+
+  abstract openDoor(key: Key): void;
+
+  comeIn(person: Person): void {
+    if (this.door === true) {
+      this.tenants.push(person);
+      console.log(`${person.getKey().getSignature()} entered the house.`);
+    } else {
+      console.log("The door is closed.");
+    }
+  }
+}
+
+class MyHouse extends House {
+  constructor(key: Key) {
+    super(key);
+  }
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("The door is opened.");
+    } else {
+      console.log("Can not open the door.");
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +66,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
